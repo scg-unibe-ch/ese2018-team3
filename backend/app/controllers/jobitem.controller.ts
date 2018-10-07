b64ed68babdcb1cb385aa6f3d28f4820dbbf519f
joeftiger <j.oeftiger@protonmail.com>
2018-10-07 13:30:01 +0200

    fix lines for readability

--- backend/app/controllers/jobitem.controller.ts
+++ backend/app/controllers/jobitem.controller.ts
@@ -3,9 +3,11 @@ import {JobList} from '../models/joblist.model';
 import {JobItem} from '../models/jobitem.model';
 
 const router: Router = Router();
+
 router.get('/', async (req: Request, res: Response) => {
 	const jobListId = parseInt(req.query.jobListId);
 	let options = {};
+
 	if (jobListId != null) {
 		options = {
 			include: [{
@@ -17,19 +19,24 @@ router.get('/', async (req: Request, res: Response) => {
 		};
 	}
 	const instances = await JobItem.findAll(options);
+
 	res.statusCode = 200;
 	res.send(instances.map(e => e.toSimplification()));
 });
+
 router.post('/', async (req: Request, res: Response) => {
 	const instance = new JobItem();
 	instance.fromSimplification(req.body);
 	await instance.save();
+
 	res.statusCode = 201;
 	res.send(instance.toSimplification());
 });
+
 router.get('/:id', async (req: Request, res: Response) => {
 	const id = parseInt(req.params.id);
 	const instance = await JobItem.findById(id);
+
 	if (instance == null) {
 		res.statusCode = 404;
 		res.json({
@@ -37,12 +44,15 @@ router.get('/:id', async (req: Request, res: Response) => {
 		});
 		return;
 	}
+
 	res.statusCode = 200;
 	res.send(instance.toSimplification());
 });
+
 router.put('/:id', async (req: Request, res: Response) => {
 	const id = parseInt(req.params.id);
 	const instance = await JobItem.findById(id);
+
 	if (instance == null) {
 		res.statusCode = 404;
 		res.json({
@@ -50,14 +60,18 @@ router.put('/:id', async (req: Request, res: Response) => {
 		});
 		return;
 	}
+
 	instance.fromSimplification(req.body);
 	await instance.save();
+
 	res.statusCode = 200;
 	res.send(instance.toSimplification());
 });
+
 router.delete('/:id', async (req: Request, res: Response) => {
 	const id = parseInt(req.params.id);
 	const instance = await JobItem.findById(id);
+
 	if (instance == null) {
 		res.statusCode = 404;
 		res.json({
@@ -65,8 +79,10 @@ router.delete('/:id', async (req: Request, res: Response) => {
 		});
 		return;
 	}
+
 	instance.fromSimplification(req.body);
 	await instance.destroy();
+
 	res.statusCode = 204;
 	res.send();
 });

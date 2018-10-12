103846dc81d24a1c7f46a16d92c2cb00f5a1c45e
Brian6330 <brian@schweigler.org>
2018-10-12 16:01:46 +0200

    Added chnaged files for Heroku

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {JobList} from './job-list';
 import {JobItem} from './job-item';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
 
 @Component({
 	selector: 'app-root',
@@ -9,21 +10,23 @@ import {HttpClient} from '@angular/common/http';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-	jobList: JobList = new JobList(null, '');
+	baseUrl = environment.baseUrl;
+
+  jobList: JobList = new JobList(null, '');
 	jobLists: JobList[] = [];
 
 	constructor(private httpClient: HttpClient) {
-
+   // this.baseUrl = environment.baseUrl;
 	}
 
 	ngOnInit() {
-		this.httpClient.get('http://localhost:3000/joblist').subscribe((instances: any) => {
+		this.httpClient.get(this.baseUrl + '/joblist').subscribe((instances: any) => {
 			this.jobLists = instances.map((instance) => new JobList(instance.id, instance.name));
 		});
 	}
 
 	onJobListCreate() {
-		this.httpClient.post('http://localhost:3000/joblist', {
+		this.httpClient.post(this.baseUrl + '/joblist', {
 			'name': this.jobList.name
 		}).subscribe((instance: any) => {
 			this.jobList.id = instance.id;

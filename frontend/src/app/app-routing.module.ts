6421c7f15fb28c815b26748363e1b28c031cdee6
Brian6330 <brian@schweigler.org>
2018-10-28 21:28:14 +0100

    Edited app-routing.module.ts, but not used yet

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { JobListComponent }   from './job-list/job-list.component';
+import { JobComponent }      from './job/job.component';
+import { JobItemComponent}  from './job-item/job-item.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'jobList', component: JobListComponent },
+
+  { path: 'jobs', component: JobComponent },
+  { path: 'detail/:jobListId', component: JobItemComponent}
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

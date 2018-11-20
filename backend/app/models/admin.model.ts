60d88489802279422d7bdfb32378ac63da330e71
Julius Oeftiger <j.oeftiger@protonmail.com>
2018-11-20 23:51:45 +0100

    Backend: Rename Admin to AdminModel for consistency

--- backend/app/models/admin.model.ts
+++ backend/app/models/admin.model.ts
@@ -2,20 +2,20 @@ import {Column, ForeignKey, Model, Table} from 'sequelize-typescript';
 import {User} from './';
 
 @Table
-export class Admin extends Model<Admin> {
+export class AdminModel extends Model<AdminModel> {
 
-	@ForeignKey(() => User)
-	@Column
-	userId!: number;
+    @ForeignKey(() => User)
+    @Column
+    userId!: number;
 
-	toSimplification(): any {
-		return {
-			'id': this.id,
-			'userId': this.userId,
-		};
-	}
+    toSimplification(): any {
+        return {
+            'id': this.id,
+            'userId': this.userId,
+        };
+    }
 
-	fromSimplification(simplification: any): void {
-		this.userId = simplification['userId'];
-	}
+    fromSimplification(simplification: any): void {
+        this.userId = simplification['userId'];
+    }
 }

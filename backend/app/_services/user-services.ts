cd21f7439ce2f122c52ee7acf96d5481290eb941
Julius Oeftiger <j.oeftiger@protonmail.com>
2018-11-22 16:53:50 +0100

    Backend: Temporary fix, saving expiration in token

--- backend/app/_services/user-services.ts
+++ backend/app/_services/user-services.ts
@@ -5,7 +5,6 @@ import {
 	UserNotAdminError,
 	UserNotApprovedError,
 	UserNotFoundError,
-	UserNotLoggedInError
 } from '../errors';
 import jwt from 'jwt-simple';
 import moment from 'moment';
@@ -15,25 +14,8 @@ import {TokenExpiredError} from '../errors/TokenExpiredError';
 export class UserServices {
 
 	private static TOKEN_SECRET: string = '%Bw[HNtju_lo:N{o&mKEcKc\"Qf5d1NH$w$EWz>UY;aS&$-wnNXx1&zqTVE8tWR&';
-	private static EXPIRES_IN: number = 30;
-	private static EXPIRES_TYPE: string = 'minutes';
 
 	constructor() {
-	}
-
-	/**
-	 * Tries to authenticate the specified user as an admin.
-	 * @param user the user to be authenticated
-	 */
-	static async authAdmin(user: any): Promise<AdminModel> {
-		const admin = await AdminModel.findOne({
-			where: {
-				userId: user.id
-			}
-		});
-
-		if (!admin) throw new UserNotAdminError();
-		return admin;
 	}
 
     /**
@@ -41,16 +23,16 @@ export class UserServices {
      * @param auth_header the token to be authenticated
      * @param asAdmin if the token should be validated as admin
      */
-	static async authenticate(auth_header: string, asAdmin: boolean = false): Promise<UserModel> {
+	static async authenticate(auth_header: string | undefined, asAdmin: boolean = false): Promise<UserModel> {
+		if (!auth_header) throw Error.prototype;
 		// Ommit leading 'Bearer'
 		const tok = auth_header.split(" ")[1];
 
         const payload = jwt.decode(tok, this.TOKEN_SECRET);
-        const userId = payload.sub;
-        const future = moment(payload.iat).add(30, 'minutes').unix();
         const now = moment().unix();
+        if (now > payload.exp) throw new TokenExpiredError();
 
-        if (now > future) throw new TokenExpiredError();
+        const userId = payload.sub;
 
 		if (asAdmin) {
             const admin = await AdminModel.findOne({
@@ -85,7 +67,7 @@ export class UserServices {
 		if (!u.isApproved) throw new UserNotApprovedError();
 
 		const payload = {
-			iat: moment().unix(),	// issued at
+			exp: moment().add(30, 'm').unix(),	// expires on
 			sub: u.id				// subject
 		};
 

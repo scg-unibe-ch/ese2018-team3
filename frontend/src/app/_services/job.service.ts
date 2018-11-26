1e27d87182a6c5d4c1e4c3a173b96ee3cb312d69
Julius Oeftiger <j.oeftiger@protonmail.com>
2018-11-26 20:45:23 +0100

    Frontend: Create Job Services

--- frontend/src/app/_services/job.service.ts
+++ frontend/src/app/_services/job.service.ts
@@ -1,4 +1,6 @@
 import {Injectable, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Job} from '../_models';
 
 /**
  * The user service contains a standard set of CRUD methods for managing users.
@@ -6,11 +8,30 @@ import {Injectable, OnInit} from '@angular/core';
  */
 @Injectable({providedIn: 'root'})
 export class JobService implements OnInit {
+    
+    private jobsUrl: string = 'http://localhost:3000/jobs/';
 
     constructor(
+        private http: HttpClient
     ) {
     }
 
     ngOnInit(): void {
+    }
+
+    getAll() {
+        return this.http.get<Job[]>(this.jobsUrl);
+    }
+
+    getById(id: number) {
+        return this.http.get(this.jobsUrl + `id/${{id}}`);
+    }
+
+    getByUserId(id: number) {
+        return this.http.get(this.jobsUrl + `user/${{id}}`);
+    }
+
+    getByCompany(company: string) {
+        return this.http.get(this.jobsUrl + `company/${{company}}`);
     }
 }

49fa4a50466fcd38b01b58a2476520a47476a267
Julius Oeftiger <j.oeftiger@protonmail.com>
2018-11-27 16:21:42 +0100

    Frontend: Introduced Component for unaproved users

---
+++ frontend/src/app/admin-panel/users-panel/users-unapproved/users-unapproved.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../../_models';
+import {AdminService} from '../../../_services';
+
+@Component({
+    selector: 'app-user-unapproved',
+    templateUrl: './users-unapproved.component.html',
+    styleUrls: ['./users-unapproved.component.css']
+})
+export class UsersUnapprovedComponent implements OnInit {
+
+    users: User[];
+
+    constructor(
+        private adminService: AdminService
+    ) {
+    }
+
+    ngOnInit() {
+        this.users = [];
+        this.loadAllUnapproved()
+    }
+
+    private loadAllUnapproved() {
+        this.adminService.getAllUnapproved().subscribe(users => {
+            this.users = users;
+        });
+    }
+}

4638202033b3aafad16e8380a76b2ffd8dfca0c7
Sara Kraettli <sara.kraettli@students.unibe.ch>
2018-12-10 22:10:12 +0100

    Implemented first version of nghtTheme

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -11,6 +11,7 @@ export class NavbarComponent implements OnInit {
 
     loggedIn: string;
     isAdmin: boolean;
+    isNight: boolean;
 
     constructor(
         private adminService: AdminService,
@@ -23,6 +24,9 @@ export class NavbarComponent implements OnInit {
     ngOnInit() {
         const header = document.getElementById('topnav');
         const btns = header.getElementsByClassName('w3-button');
+        if (this.isNight){
+          this.changeDesignToNightTheme();
+        }
         for (let i = 0; i < btns.length; i++) {
             btns[i].addEventListener('click', function () {
                 var current = document.getElementsByClassName('active');
@@ -35,5 +39,52 @@ export class NavbarComponent implements OnInit {
     logout() {
         this.userService.logout();
         this.router.navigate(['/home']);
+    }
+
+  switchToNightTheme() {
+      if (this.isNight) {
+        this.isNight = false;
+        this.changeDesignToDayTheme();
+      } else {
+        this.isNight = true;
+        this.changeDesignToNightTheme();
+      }
+
+
+    }
+
+    changeDesignToDayTheme() {
+      const body = document.getElementById("body");
+      const footer = document.getElementById("footer");
+      const sidebar = document.getElementById("sidenav");
+
+      document.body.style.backgroundColor = "white";
+
+
+      body.classList.remove("w3-black")
+      body.classList.add("w3-white");
+
+      footer.classList.remove("w3-black")
+      footer.classList.add("w3-white");
+
+      sidebar.classList.remove("w3-dark-gray")
+      sidebar.classList.add("w3-light-gray");
+    }
+
+    changeDesignToNightTheme() {
+      const body = document.getElementById("body");
+      const footer = document.getElementById("footer");
+      const sidebar = document.getElementById("sidenav");
+
+      document.body.style.backgroundColor = "black";
+
+      body.classList.remove("w3-white")
+      body.classList.add("w3-black");
+
+      footer.classList.remove("w3-white")
+      footer.classList.add("w3-black");
+
+      sidebar.classList.remove("w3-light-gray")
+      sidebar.classList.add("w3-dark-gray");
     }
 }

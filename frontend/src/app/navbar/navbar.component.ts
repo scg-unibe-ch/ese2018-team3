448ea3fac877e8d9ed5b530bb0e0db92146c14b1
Julius Oeftiger <j.oeftiger@protonmail.com>
2018-12-11 15:13:52 +0100

    Frontend: Remove unused variable

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -10,7 +10,6 @@ import {Router} from '@angular/router';
 export class NavbarComponent implements OnInit {
 
     loggedIn: string;
-    isAdmin: boolean;
 
     constructor(
         private adminService: AdminService,

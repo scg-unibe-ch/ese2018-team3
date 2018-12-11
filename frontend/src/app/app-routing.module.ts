ebdca4a79d8a49fe3f76532467f83f3df394631c
Julius Oeftiger <j.oeftiger@protonmail.com>
2018-12-11 10:12:57 +0100

    Frontend: Style fixes, improved readability

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -22,35 +22,32 @@ import {AdminJobsComponent} from './admin-panel/admin-jobs/admin-jobs.component'
 import {AdminJobEditorComponent} from './admin-panel/admin-jobs/admin-job-editor/admin-job-editor.component';
 
 const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
+    {path: '',  redirectTo: '/home',    pathMatch: 'full'},
+
+    {path: 'home',  component: HomeComponent},
     {path: 'about', component: AboutComponent},
 
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
+    {path: 'login',     component: LoginComponent},
+    {path: 'register',  component: RegisterComponent},
 
-    {path: 'jobs', component: JobOffersComponent},
-    {path: 'jobs/:id', component: JobDetailComponent},
-    {path: 'job-creator', component: JobCreatorComponent, canActivate: [AuthGuard]},
-    {path: 'my-jobs-panel', component: MyJobsPanelComponent, canActivate: [AuthGuard]},
+    {path: 'jobs',          component: JobOffersComponent},
+    {path: 'jobs/:id',      component: JobDetailComponent},
+    {path: 'job-creator',   component: JobCreatorComponent,     canActivate: [AuthGuard]},
+    {path: 'my-jobs-panel', component: MyJobsPanelComponent,    canActivate: [AuthGuard]},
 
-    {path: 'admin-panel', component: AdminPanelComponent, canActivate: [AuthGuard, AdminAuthGuard]},
-    {path: 'admin-panel/users', component: UsersPanelComponent, canActivate: [AuthGuard]},
-    {
-        path: 'admin-panel/users-unapproved',
-        component: UsersUnapprovedComponent,
-        canActivate: [AuthGuard, AdminAuthGuard]
-    },
-    {path: 'admin-panel/users/edit/:id', component: UserEditComponent, canActivate: [AuthGuard, AdminAuthGuard]},
-    {path: 'admin-panel/jobs-unapproved', component: JobsUnapprovedComponent, canActivate: [AuthGuard, AdminAuthGuard]},
-    {path: 'admin-panel/jobs', component: AdminJobsComponent, canActivate: [AuthGuard]},
-    {path: 'admin-panel/jobs/edit/:id', component: AdminJobEditorComponent, canActivate: [AuthGuard, AdminAuthGuard]},
+    {path: 'admin-panel',                   component: AdminPanelComponent,         canActivate: [AuthGuard]},
+    {path: 'admin-panel/users',             component: UsersPanelComponent,         canActivate: [AuthGuard]},
+    {path: 'admin-panel/users-unapproved',  component: UsersUnapprovedComponent,    canActivate: [AuthGuard]},
+    {path: 'admin-panel/users/edit/:id',    component: UserEditComponent,           canActivate: [AuthGuard]},
+    {path: 'admin-panel/jobs-unapproved',   component: JobsUnapprovedComponent,     canActivate: [AuthGuard]},
+    {path: 'admin-panel/jobs',              component: AdminJobsComponent,          canActivate: [AuthGuard]},
+    {path: 'admin-panel/jobs/edit/:id',     component: AdminJobEditorComponent,     canActivate: [AuthGuard]},
 
     //{path: 'job/new', component: ProfileNewJobComponent},
     //{path: 'profile', component: JobManagementComponent},
 
     // otherwise redirect to home
-    {path: '**', redirectTo: '/home'}
+    {path: '**',    redirectTo: '/home'}
 ];
 
 export const AppRoutingModule = RouterModule.forRoot(routes, {
